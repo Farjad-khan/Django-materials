136a01644e3700a6f123d67d83229724cb63c5c1
Farjad Ahmed Khan <71921754+Farjad-khan@users.noreply.github.com>
2021-04-24 15:24:59 +0500

    forms

---
+++ forms/enroll/forms.py
@@ -0,0 +1,8 @@
+from django.core import validators
+from django import forms
+class StudentRegistration(forms.Form):
+    error_css_class = 'error'
+    required_css_class = 'required'
+    name = forms.CharField(error_messages={'required':'Enter your name'})
+    email = forms.EmailField(error_messages={'required':'Enter your Email'})
+    password = forms.CharField(widget=forms.PasswordInput, error_messages={'required':'Enter Password'})
