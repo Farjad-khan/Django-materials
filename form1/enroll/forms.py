96876ff8a460c2f25952d7c7cf82466631472714
Farjad Ahmed Khan <71921754+Farjad-khan@users.noreply.github.com>
2021-04-24 15:24:15 +0500

    form1

---
+++ form1/enroll/forms.py
@@ -0,0 +1,7 @@
+from django.core import validators
+from django import forms
+class StudentRegistration(forms.Form):
+   
+    name = forms.CharField(error_messages={'required':'Enter your name'})
+    email = forms.EmailField(error_messages={'required':'Enter your Email'})
+    password = forms.CharField(widget=forms.PasswordInput, error_messages={'required':'Enter Password'})
---
+++ form1/enroll/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .forms import StudentRegistration
+from .models import User
+# Create your views here.
+
+def showformdata(request):
+    if request.method == 'POST':
+        fm = StudentRegistration(request.POST)
+        if fm.is_valid():
+            nm = fm.cleaned_data['name']
+            em = fm.cleaned_data['email']
+            pwd = fm.cleaned_data['password']
+            reg = User(id = 1,name=nm, email=em, password=pwd)
+            reg.save()
+    else:
+        fm = StudentRegistration()
+
+    return render(request, 'enroll/userRegistration.html', {'form':fm})
